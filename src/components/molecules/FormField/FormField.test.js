0828ed544af9fac4e25c9d871098b96d52e07d31
Paweł Uchański <uchanski.pawel@gmail.com>
2022-04-15 23:50:34 +0200

    Learn basics of testing in jest and react testing library

---
+++ src/components/molecules/FormField/FormField.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { renderWithProviders } from "helpers/renderWithProviders";
+import FormField from "./FormField";
+
+describe('Form Field', () => {
+  it('Renders the component', () => {
+    renderWithProviders(<FormField label="name" name="name" id="name" />);
+  });
+});
